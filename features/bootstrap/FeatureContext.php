53822e6fa269db5af24aa79ecb35f40598b53105
marcosQuesada <marcos.quesadas@gmail.com>
2012-12-15 00:49:20 +0100

    first commit

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,44 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\MinkExtension\Context\MinkContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+//
+// Require 3rd-party libraries here:
+//
+//   require_once 'PHPUnit/Autoload.php';
+//   require_once 'PHPUnit/Framework/Assert/Functions.php';
+//
+
+/**
+ * Features context.
+ */
+class FeatureContext extends MinkContext
+{
+    /**
+     * Initializes context.
+     * Every scenario gets it's own context object.
+     *
+     * @param array $parameters context parameters (set them up through behat.yml)
+     */
+    public function __construct(array $parameters)
+    {
+        // Initialize your context here
+    }
+    /**
+     * @Given /^I am logged in as "([^"]*)" with password "([^"]*)"$/
+     */
+    public function iAmLoggedInAsWithPassword($arg1, $arg2)
+    {
+        $this->visit("/demo/secured/login");
+        $this->fillField("username", $arg1);
+        $this->fillField("password", $arg2);
+        $this->pressButton("LOGIN");
+    }
+
+}
